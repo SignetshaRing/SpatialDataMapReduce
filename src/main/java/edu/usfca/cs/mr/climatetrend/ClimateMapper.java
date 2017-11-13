37fbaef39d6612b461ec2d65b7f51cfc479d2a65
cmattey <cmattey@dons.usfca.edu>
2017-11-13 01:45:32 -0800

    Added climate trend job

---
+++ src/main/java/edu/usfca/cs/mr/climatetrend/ClimateMapper.java
@@ -0,0 +1,59 @@
+package edu.usfca.cs.mr.climatetrend;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+/**
+ * Mapper: Reads line by line, split them into words. Emit <word, 1> pairs.
+ */
+public class ClimateMapper
+extends Mapper<LongWritable, Text, Text, Text> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+        // tokenize into words.
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        // emit word, count pairs.
+        int index = 0;
+        String timestamp = "";
+        String target_geohash = "9q8ytxu5et";
+        String geohash = "";
+        String temp = "";
+        String precip = "";
+        while (itr.hasMoreTokens()) {
+            String token = itr.nextToken();
+            if(index == 0)
+                timestamp = token;
+            else if(index == 1)
+                geohash = token;
+            else if(index == 40)
+            {
+                temp = token;
+//                String record = timestamp+","+geohash+","+temp;
+//                context.write(new Text("record"), new Text(record));
+            }
+            else if(index == 55)
+            {
+                precip = token;
+                if(target_geohash.equals(geohash))
+                {
+                    Date date = new Date(Long.parseLong(timestamp));
+                    DateFormat format = new SimpleDateFormat("MM/yyyy");
+                    String date_format = format.format(date);
+
+                    String record = geohash+","+temp+","+precip;
+                    context.write(new Text(date_format), new Text(record));
+                }
+            }
+            index++;
+        }
+    }
+}
---
+++ src/main/java/edu/usfca/cs/mr/climatetrend/ClimateReducer.java
@@ -0,0 +1,79 @@
+package edu.usfca.cs.mr.climatetrend;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Reducer: Input to the reducer is the output from the mapper. It receives
+ * word, list<count> pairs.  Sums up individual counts per given word. Emits
+ * <word, total count> pairs.
+ */
+public class ClimateReducer
+extends Reducer<Text, Text, Text, Text> {
+
+    @Override
+    protected void reduce(
+            Text key, Iterable<Text> values, Context context)
+    throws IOException, InterruptedException {
+        List<Float> tempList = new ArrayList<>();
+        List<Float> precipList = new ArrayList<>();
+        String geohash ="";
+
+        for(Text record : values){
+            String rec = record.toString();
+            List<String> data = Arrays.asList(rec.split(","));
+            geohash = data.get(0);
+            Float temp = Float.parseFloat(data.get(1));
+            Float precip = Float.parseFloat(data.get(2));
+
+            tempList.add(temp);
+            precipList.add(precip);
+        }
+
+        Collections.sort(tempList);
+        Collections.sort(precipList);
+
+        Float minTemp = tempList.get(0);
+        Float maxTemp = tempList.get(tempList.size()-1);
+
+        Float minPrecip = precipList.get(0);
+        Float maxPrecip = precipList.get(precipList.size()-1);
+
+        Float tsum = 0f;
+        for(Float t : tempList)tsum+=t;
+        Float avgTemp = tsum/tempList.size();
+
+        Float psum = 0f;
+        for(Float p : precipList)psum+=p;
+        Float avgPrecip = psum/precipList.size();
+
+        //Output to File Format
+        // <month-num>  <high-temp>  <low-temp>  <avg-precip>  <avg-temp>
+
+        int month_num = Integer.parseInt(key.toString().substring(0,2));
+
+        String output = month_num+" "+maxTemp+" "+minTemp+" "+avgPrecip+" "+avgTemp;
+
+        String file_name = "Climate_Trend_"+geohash+".txt";
+        try(FileWriter fw = new FileWriter(file_name, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(output);
+        } catch (IOException e) {
+            System.out.println("Error while writing to File");
+        }
+
+        context.write(key, new Text(output));
+    }
+
+}
